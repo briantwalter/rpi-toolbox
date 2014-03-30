b0d751686c93b4823fbcb7ffd1960431284439d0
briantwalter <walter.brian@gmail.com>
2014-03-29 22:47:14 -0700

    photomon in the works

--- photomon.py
+++ photomon.py
@@ -5,23 +5,50 @@
 # control the hdmi port based on values from a photocell
 #
  
-import RPi.GPIO as GPIO, time, os      
- 
+import os      
+import time
+import math
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# debug settings
 DEBUG = 1
+# configuration variables
+gpiopin = 22	# gpio pin on the rpi
+samples = 4 	# number of loop iterations
+interval = 1 	# in seconds or partial seconds
+hdmictl = "/home/pi/rpi-toolbox/hdmictl"
+
+# start at a known state and turn on the hdmi
+os.system("su - pi -c \"" + hdmictl + " on\"")
+
 GPIO.setmode(GPIO.BCM)
  
-def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        #time.sleep(0.1)
-        time.sleep(0.5)
+def readpin (readpin):
+    reading = 0
+    GPIO.setup(readpin, GPIO.OUT)
+    GPIO.output(readpin, GPIO.LOW)
+    time.sleep(interval)
+    GPIO.setup(readpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while (GPIO.input(readpin) == GPIO.LOW):
+      reading += 1
+    return reading
  
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
-        return reading
- 
-while True:                                     
-        print RCtime(22)     # Read RC timing using pin #22
+def getaverage ():
+  sum = 0
+  for n in range(0, samples):
+    sum = sum + readpin(gpiopin)
+    print "DEBUG: sum is " + str(sum)
+  else:
+    print "DEBUG: total sum is " + str(sum)
+    average = sum / samples
+    print "DEBUG: average is " + str(average)
+    return average
+
+newaverage = getaverage()
+print "DEBUG: newaverage is " + str(newaverage)
+
+
+
+
